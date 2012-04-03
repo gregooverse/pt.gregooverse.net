f425bddcd4d4e3235030c1f293d8225ea344ef2a
Gregoo <gregoo@gregooverse.net>
2012-04-03 23:06:05 +0200

    Removed embedded tabs, was too much

--- mod/dll.php
+++ mod/dll.php
@@ -1,19 +1,19 @@
 <?php if(!defined('PT_GREGOOVERSE_NET')) exit; ?>
     <div class="accordion">
         <h3><a href="#">Data submitted</a></h3>
-        <div>
+        <p>
             The data submitted is <b>NOT</b> saved. It's injected inside the .dlls and then destroyed. The file you download isn't saved either.<br />
             If you are not sure, enter dummy data, and edit it manually after with an hexadecimal editor. You can find the offsets in the sections below :).
-        </div>
+        </p>
         <h3><a href="#">About the .dll</a></h3>
-        <div>
+        <p>
             The 3 files (clan.dll, clan-procedure.dll and sql.dll) are cleaned up version. The syntax has been changed to fit a MSSQL 2005 (or above) server.<br />
             The database name is hardcoded inside the files, you won't need any registry value.<br />
             These files allow you to have functionnal SoD, rankings, online list, etc..<br />
             <a href="?p=misc&amp;id=log_user_connections">More info about clan-procedure.dll</a>    
-        </div>
+        </p>
         <?php
-            $lambda = create_function('$s', 'return sprintf("0x%\'06X\n", $s);');
+            $lambda = create_function('$s', 'return sprintf("@ 0x%\'06X\n", $s);');
             foreach($conf_dll['files'] as $dll){
                 if(($data = data::read('pub/dll/' . $dll . '.dll')) !== false){
         ?>
@@ -28,61 +28,26 @@
                         <li><a href="#tabs-5">Password</a></li>
                 </ul>
                 <div id="tabs-1">
-                    <?php
+                    <pre class="brush: sql"><?php
                         $i = 0;
                         foreach($conf_dll['offsets'][$dll]['query'] as $offset){
                             $i++;
                             $query = hex::string($data, $offset);
-                    ?>
-                    <div class="tabs">
-                        <ul>
-                                <li><a href="#tabs-1-<?php echo $i ?>"><?php printf("Hexadecimal - %'06X", $offset); ?></a></li>
-                        </ul>
-                        <div id="tabs-1-<?php echo $i ?>">
-                            <pre class="brush: sql"><?php echo $query; ?></pre>
-                        </div>
-                    </div>
-                    <?php } ?>
+                            printf("@ 0x%'06X:\n%s\n\n", $offset, $query);
+                        }
+                    ?></pre>
                 </div>
                 <div id="tabs-2">
-                    <div class="tabs">
-                        <ul>
-                                <li><a href="#tabs-2-1">Hexadecimal</a></li>
-                        </ul>
-                        <div id="tabs-2-1">
-                            <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['string'])); ?></pre>
-                        </div>
-                    </div>
+                    <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['string'])); ?></pre>
                 </div>
                 <div id="tabs-3">
-                    <div class="tabs">
-                        <ul>
-                                <li><a href="#tabs-3-1">Hexadecimal</a></li>
-                        </ul>
-                        <div id="tabs-3-1">
-                            <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['instance'])); ?></pre>
-                        </div>
-                    </div>
+                    <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['instance'])); ?></pre>
                 </div>
                 <div id="tabs-4">
-                    <div class="tabs">
-                        <ul>
-                                <li><a href="#tabs-4-1">Hexadecimal</a></li>
-                        </ul>
-                        <div id="tabs-4-1">
-                            <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['username'])); ?></pre>
-                        </div>
-                    </div>
+                   <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['username'])); ?></pre>
                 </div>
                 <div id="tabs-5">
-                    <div class="tabs">
-                        <ul>
-                                <li><a href="#tabs-5-1">Hexadecimal</a></li>
-                        </ul>
-                        <div id="tabs-5-1">
-                            <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['password'])); ?></pre>
-                        </div>
-                    </div>
+                   <pre><?php echo implode(array_map($lambda, $conf_dll['offsets'][$dll]['password'])); ?></pre>
                 </div>
             </div>        
         </div>
