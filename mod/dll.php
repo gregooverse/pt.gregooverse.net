f3969140622a4247742f15c7d982eb1d0c6178ee
Gregoo <gregoo@gregooverse.net>
2012-04-04 23:15:54 +0200

    input maxlength, button value, .dlls and .zip generation

--- mod/dll.php
+++ mod/dll.php
@@ -1,9 +1,44 @@
-<?php if(!defined('PT_GREGOOVERSE_NET')) exit; ?>
+<?php 
+if(!defined('PT_GREGOOVERSE_NET')) exit;
+
+list($action, $server, $username, $password) = page::posts('action', 'dll_server', 'dll_user', 'dll_pass');
+
+if($action === "generate"){
+    if(strlen($server) <= $conf_dll['sizes']['instance'] && strlen($username) <= $conf_dll['sizes']['username'] && strlen($password) <= $conf_dll['sizes']['password']){
+        $zip = new zipfile();
+        
+        foreach($conf_dll['files'] as $dll){
+            $data = data::read('pub/dll/' . $dll . '.dll');
+            
+            if($data === false)
+                continue;
+            
+            foreach($conf_dll['offsets'][$dll]['string'] as $offset)
+                data::replace($data, $server, $offset, $conf_dll['sizes']['instance']);
+
+            foreach($conf_dll['offsets'][$dll]['instance'] as $offset)
+                data::replace($data, $server, $offset, $conf_dll['sizes']['instance']);
+
+            foreach($conf_dll['offsets'][$dll]['username'] as $offset)
+                data::replace($data, $server, $offset, $conf_dll['sizes']['username']);
+
+            foreach($conf_dll['offsets'][$dll]['password'] as $offset)
+                data::replace($data, $server, $offset, $conf_dll['sizes']['password']);
+
+            $zip->addFile($data, $dll . '.dll');
+        }
+        
+        header("Content-type: application/octet-stream");
+        header("Content-disposition: attachment; filename=dll.zip");
+        echo $zip->file();
+    }
+}
+?>
     <div class="accordion">
         <h3><a href="#">Data submitted</a></h3>
         <p>
             The data submitted is <b>NOT</b> saved. It's injected inside the .dlls and then destroyed. The file you download isn't saved either.<br />
-            If you are not sure, enter dummy data, and edit it manually after with an hexadecimal editor. You can find the offsets in the sections below :).
+            If you are not sure, enter dummy data (i don't suggest entering nothing, it will be harder to locate in your file after), and edit it manually after with an hexadecimal editor. You can find the offsets in the sections below :).
         </p>
         <h3><a href="#">About the .dll</a></h3>
         <p>
@@ -54,12 +89,13 @@
         <?php } } ?>
     </div>
 </div>
-<form method="post" action="?p=dll">
-    <label for="dll_server">SQL instance name (<?php echo $conf_dll['sizes']['instance']; ?> characters max)</label>
-    <input type="text" id="dll_server" name="dll_server" />
-    <label for="dll_user">SQL username (<?php echo $conf_dll['sizes']['username']; ?> characters max)</label>
-    <input type="text" id="dll_user" name="dll_user" />
-    <label for="dll_pass">SQL password (<?php echo $conf_dll['sizes']['password']; ?> characters max)</label>
-    <input type="text" id="dll_pass" name="dll_pass" />
-    <button id="action" type="submit">Generate .dll</button>
-</form>
+<div class="form">
+    <form method="post" action="?p=dll">
+        <label for="dll_server">SQL instance name (<?php echo $conf_dll['sizes']['instance']; ?> characters max)</label>
+        <input type="text" id="dll_server" name="dll_server" maxlength="<?php echo $conf_dll['sizes']['instance']; ?>" />
+        <label for="dll_user">SQL username (<?php echo $conf_dll['sizes']['username']; ?> characters max)</label>
+        <input type="text" id="dll_user" name="dll_user" maxlength="<?php echo $conf_dll['sizes']['username']; ?>" />
+        <label for="dll_pass">SQL password (<?php echo $conf_dll['sizes']['password']; ?> characters max)</label>
+        <input type="text" id="dll_pass" name="dll_pass" maxlength="<?php echo $conf_dll['sizes']['password']; ?>" />
+        <button name="action" type="submit" value="generate">Generate .dll</button>
+    </form>
