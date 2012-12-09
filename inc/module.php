9a82bf9166c3440615fb37b7c5b24682c1b72aa8
Gregoo <gregoo@gregooverse.net>
2012-12-09 17:43:22 +0100

    Error page when the module does not exist or is not available

--- inc/module.php
+++ inc/module.php
@@ -1,11 +1,18 @@
 <?php if(!defined('PT_GREGOOVERSE_NET')) exit; ?>
 <div class="body"><?php 
-    if(ribbon::in(page::get('p'))) 
-        if(file_exists('mod/' . page::get('p') . '.php')){
-            crumbs::page();
-            echo '<h1>', page::title(false), '</h1>';
-            require_once 'mod/' . page::get('p') . '.php'; 
+    if(page::get('p') !== false) {
+        if(ribbon::in(page::get('p'))) {
+            if(file_exists('mod/' . page::get('p') . '.php')){
+                crumbs::page();
+                echo '<h1>', page::title(false), '</h1>';
+                require_once 'mod/' . page::get('p') . '.php'; 
+            } else {
+                page::error("The module (" . page::get('p') . ") is not available yet.");
+            }
+        } else {
+            page::error("The module (" . page::get('p') . ") doesn't exist.");
         }
+    }
 ?>
     <!--<div id="disqus_thread"></div>-->
 </div>
